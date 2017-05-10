0cde0e137b75b274542d351e1ace201b9f6b7606
Jake Bladt <jake@ambulnz.com>
2017-05-10 16:51:12 -0400

    initialize

--- generator/generate.py
+++ generator/generate.py
@@ -3,9 +3,11 @@
 class StationFactory:
   def __init__(self, label):
     self.label = label
+    self.stations = {}
 
 class LineFactory:
   def __init__(self, label):
     self.label = label
+    self.lines = {}
 
 
